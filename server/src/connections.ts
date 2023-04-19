94c312bbcb483576adafded7251e92f88e35b31e
Sam Wilson <sam@binarycake.ca>
2023-04-19 15:17:54 -0400

    Make some changes to run in low RAM environments

--- server/src/connections.ts
+++ server/src/connections.ts
@@ -4,92 +4,17 @@ import { Buffer } from "node:buffer";
 import { ServerResponse } from "node:http";
 import { WebSocket } from "ws";
 
-class Pending {
-    private _bytes = 0;
-    public readonly pending: Array<[Buffer, ServerResponse]> = [];
-
-    public get bytes(): number {
-        return this._bytes;
-    }
-
-    private evict(): void {
-        const shifted = this.pending.shift();
-        if (!shifted) {
-            return;
-        }
-
-        const [buffer, response] = shifted;
-        this._bytes -= buffer.length;
-
-        console.debug("Evicting pending request");
-
-        // Send back an error.
-        response
-            .writeHead(504, "too many pending requests", {
-                "Content-Type": "application/json",
-            })
-            .end(
-                JSON.stringify({
-                    jsonrpc: "2.0",
-                    error: {
-                        code: -32603,
-                        message: "wtf: too many pending requests",
-                    },
-                    id: null,
-                })
-            );
-    }
-
-    public push(request: Buffer, response: ServerResponse): void {
-        if (request.length > Connections.MAX_BYTES) {
-            // Send back an error.
-            response
-                .writeHead(413, "request too large", {
-                    "Content-Type": "application/json",
-                })
-                .end(
-                    JSON.stringify({
-                        jsonrpc: "2.0",
-                        error: {
-                            code: -32603,
-                            message: "wtf: request too large",
-                        },
-                        id: null,
-                    })
-                );
-            return;
-        }
-
-        this.pending.push([request, response]);
-        this._bytes += request.length;
-
-        while (this._bytes > Connections.MAX_BYTES) {
-            this.evict();
-        }
-    }
-
-    public dispose(): void {
-        while (this.pending.length > 0) {
-            this.evict();
-        }
-    }
-}
-
-type MaybeClient = ClientState | Pending;
-
 export class Connections {
-    readonly connections: LRUCache<string, MaybeClient>;
-
-    public static MAX_BYTES = 16 * 1024 * 1024;
+    readonly connections: LRUCache<string, ClientState>;
 
     constructor() {
         this.connections = new LRUCache({
             max: 32,
-            dispose: this.disposeConnection,
+            dispose: (value, key) => this.disposeConnection(value, key),
         });
     }
 
-    private disposeConnection(value: MaybeClient, key: string): void {
+    private disposeConnection(value: ClientState, key: string): void {
         console.debug("Evicting connection", key);
         value.dispose();
     }
@@ -99,21 +24,30 @@ export class Connections {
         request: Buffer,
         response: ServerResponse
     ): void {
-        let maybeClient = this.connections.get(key);
+        const maybeClient = this.connections.get(key);
 
-        if (maybeClient instanceof ClientState) {
+        if (maybeClient) {
             console.debug("Proxying request", key);
             maybeClient.request(request, response);
-            return;
-        }
+        } else {
+            console.debug("Rejecting request (no WebSocket)", key);
 
-        if (!maybeClient) {
-            maybeClient = new Pending();
-            this.connections.set(key, maybeClient);
+            // Send back an error.
+            response
+                .writeHead(502, "no WebSocket connection", {
+                    "Content-Type": "application/json",
+                })
+                .end(
+                    JSON.stringify({
+                        jsonrpc: "2.0",
+                        error: {
+                            code: -32603,
+                            message: "wtf: no WebSocket connection",
+                        },
+                        id: null,
+                    })
+                );
         }
-
-        console.debug("Queuing request", key);
-        maybeClient.push(request, response);
     }
 
     public wsConnect(key: string, ws: WebSocket): void {
@@ -123,18 +57,12 @@ export class Connections {
         const state = new ClientState(ws);
         const maybeClient = this.connections.get(key);
 
-        if (maybeClient instanceof ClientState) {
+        if (maybeClient) {
             // Assume the existing connection is the "real" one.
             ws.close();
-            return;
-        } else if (maybeClient instanceof Pending) {
-            // Send pending requests.
-            for (const [request, response] of maybeClient.pending) {
-                state.request(request, response);
-            }
+        } else {
+            this.connections.set(key, state);
         }
-
-        this.connections.set(key, state);
     }
 
     private delete(key: string): void {
