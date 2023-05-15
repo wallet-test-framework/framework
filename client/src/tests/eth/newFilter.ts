c8f361d4057f251abc6d313feebb4f82c3f77abe
Nikki Gaudreau <nikki@binarycake.ca>
2023-05-15 12:08:29 -0400

    test: newFilter doesn't return events

--- client/src/tests/eth/newFilter.ts
+++ client/src/tests/eth/newFilter.ts
@@ -1,4 +1,5 @@
 import * as tests from "../../tests";
+import { notEver } from "../../util";
 import newFilter from "./newFilter.sol";
 import assert from "assert";
 import { ethers } from "ethers";
@@ -12,6 +13,7 @@ if (!blockchain || !wallet) {
 
 describe("newFilter", () => {
     let contract: ethers.Contract;
+    let contract2: ethers.Contract;
 
     before(async () => {
         const deployer = (await blockchain.listAccounts())[0];
@@ -21,8 +23,10 @@ describe("newFilter", () => {
         );
 
         contract = await factory.deploy();
+        contract2 = await factory.deploy();
         await blockchain.send("evm_mine", [{ blocks: 1 }]);
         await contract.deploymentTransaction()?.wait(1);
+        await contract2.deploymentTransaction()?.wait(1);
     });
 
     it("returns events matching filter", async () => {
@@ -34,5 +38,15 @@ describe("newFilter", () => {
         await call.wait(1);
         const actual = await eventPromise;
         assert.equal(actual, 1234n);
+    });
+
+    it("doesn't return events from different contract", async () => {
+        const eventPromise = new Promise(
+            (resolve) => void contract2.once("Log", (args) => resolve(args))
+        );
+        const call = await contract.logSomething(1234n);
+        await blockchain.send("evm_mine", [{ blocks: 1 }]);
+        await call.wait(1);
+        await notEver(eventPromise);
     });
 });
