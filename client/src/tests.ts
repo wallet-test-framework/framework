a727791f50613c91a90b9456b6af1b20d64ceab4
Nikki Gaudreau <nikki@binarycake.ca>
2023-06-02 18:37:04 -0400

    test: getCode, getStorageAt

--- client/src/tests.ts
+++ client/src/tests.ts
@@ -11,7 +11,7 @@ export async function run(myBlockchain: TestChain, myWallet: Chain) {
     mocha.setup({
         ui: "bdd",
         timeout: 10 * 60 * 1000,
-        slow: 60100
+        slow: 60100,
     });
     await import("./tests/eth/getBlockByHash");
     await import("./tests/eth/getBlockByNumber");
@@ -22,6 +22,7 @@ export async function run(myBlockchain: TestChain, myWallet: Chain) {
     await import("./tests/eth/newFilter");
     await import("./tests/eth/newBlockFilter");
     await import("./tests/eth/getBalance");
+    await import("./tests/eth/getStorageAt");
     await import("./tests/eth/getTransactionCount");
     await import("./tests/eth/getCode");
     await import("./tests/eth/getTransactionByHash");
